b9f98ebb097bef0dc8864e277fd0dd10c2e4b0c7
Nate Schultz <ngs32@drexel.edu>
2021-08-05 15:27:11 -0400

    prob7 answer go

---
+++ problems/prob7 - 10001st prime/sln.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	seive := sieve_of_eratosthenes(500000)
+	fmt.Println(seive[10001])
+}
+
+func sieve_of_eratosthenes(n int) []int {
+	sieve := make_range(n)
+	for i := 2; i < int(math.Sqrt(float64(n))); i++ {
+		criteria := func(ii int) bool {
+			return ii == i || ii % i != 0
+		}
+		sieve = filter(sieve, criteria)
+	}
+
+	// return the last (largest) prime
+	return sieve;
+}
+
+func make_range(size int) []int {
+	arr := make([]int, size)
+	for i := range arr {
+		arr[i] = i
+	}
+	return arr
+}
+
+func filter(arr []int, criteria func(int) bool) []int {
+	ret := make([]int, 0)
+	for _, i := range arr {
+		if criteria(i) {
+			ret = append(ret, i)
+		}
+	}
+	return ret
+}
